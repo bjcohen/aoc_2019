1aca3115bc898b255dd4921be78d96aa11a7e6d5
Ben Cohen <ben@cohen-family.org>
2024-11-26 12:27:26 -0500

    day1

---
+++ day1.py
@@ -0,0 +1,12 @@
+def fuel(m):
+    if m <= 8:
+        return 0
+    f = m // 3 - 2
+    return f + fuel(f)
+
+
+if __name__ == '__main__':
+    with open('day1.txt') as f:
+        print(sum(int(l) // 3 - 2 for l in f))
+    with open('day1.txt') as f:
+        print(sum(fuel(int(l)) for l in f))
